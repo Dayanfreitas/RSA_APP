0c270a9752272329787742f14081c6d81f33b62d
Dayanfreitas <dayan.freitas@catolicasc.org.br>
2022-07-08 21:25:35 -0300

    remover

--- src/encripta.rb
+++ src/encripta.rb
@@ -5,10 +5,6 @@ require_relative 'utils/files'
 
 params = read_args(args: ARGV)
 
-puts "path_keys => #{params.path_keys}"
-puts "path_name_file_input => #{params.path_name_file_input}"
-puts "path_name_file_output => #{params.path_name_file_output}"
-
 file_keys = Files.new(params.path_keys)
 file_input = Files.new(params.path_name_file_input)
 file_output = Files.new(params.path_name_file_output)
